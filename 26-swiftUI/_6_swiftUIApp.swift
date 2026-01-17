1432d4a2532ac7748c8ad8c96e493c64d0a6a2e0
selkain-dev <selkain@yahoo.com>
2026-01-17 12:25:00 +0100

    Initial Commit

---
+++ 26-swiftUI/_6_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _6_swiftUIApp.swift
+//  26-swiftUI
+//
+//  Created by S on 17/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct _6_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
