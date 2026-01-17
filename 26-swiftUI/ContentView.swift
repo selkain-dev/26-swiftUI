c92ad2805c98f6676bdd805d3665799496099493
selkain-dev <selkain@yahoo.com>
2026-01-17 12:34:10 +0100

    start

--- 26-swiftUI/ContentView.swift
+++ 26-swiftUI/ContentView.swift
@@ -8,12 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State  var name: String = "John"
+    @State var score: Int = 114
     var body: some View {
+     
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            let name2 =  "Denis"
+            Button ("change name"){
+                name = "Eddie"
+            }
+            
+            Button("change score"){
+                score =  223}
+            if score > 114{
+                Text("hej \(name) and \(name2) (oops hacker here), your score is: \(score).")
+            }
+            else{
+                Text("hej \(name), your score is: \(score).")
+            }
+            
+            //tablice, słowniki klucz -> wartość, zbiory - unikalne elementy
+            let colors = ["red", "green", "blue"]
+       
         }
         .padding()
     }
